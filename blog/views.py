d57684feef82b4d094dcceca70383a3194ba7c40
smallchocho <bigchocho@gmail.com>
2016-05-28 14:04:27 +0800

    Nikita Shy blog

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,27 @@
+from django.shortcuts import redirect
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {"posts":posts})
+    #從資料庫拉出Post資料放進變數posts中,並請求打開'blog/post_list.html'#
+    #然後將剛剛的變數post放到一個{"posts":posts}的template變數裡頭，準備給post_list.html使用#
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
